3a93fbdd233827968cfe533fd103e0367cb7a64d
cutmail <sunday.is.over@gmail.com>
2012-02-16 13:51:28 +0900

    Initial Commit

---
+++ TWAccountList/AppDelegate.h
@@ -0,0 +1,19 @@
+//
+//  AppDelegate.h
+//  TWAccountList
+//
+//  Created by 荒井 達哉 on 12/02/16.
+//  Copyright (c) 2012年 genesix, Inc. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@class ViewController;
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@property (strong, nonatomic) ViewController *viewController;
+
+@end
